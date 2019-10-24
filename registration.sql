154df9af7e55849f0bfca4358ac625098f1e4114
DusanPejovic <49121565+DusanPejovic@users.noreply.github.com>
2019-10-25 01:32:03 +0200

    Add files via upload

--- registration.sql
+++ registration.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Oct 23, 2019 at 01:54 PM
+-- Generation Time: Oct 24, 2019 at 11:31 PM
 -- Server version: 5.7.26
 -- PHP Version: 7.2.18
 
@@ -42,12 +42,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
-(1, 'tektronix', 'asd@asd.com', 'd41d8cd98f00b204e9800998ecf8427e'),
-(2, 'senzori', 'bflacebouker@gmail.com', 'e10adc3949ba59abbe56e057f20f883e'),
-(3, 'qweqeqwe', 'qweqwew@gdasd.com', 'a8f5f167f44f4964e6c998dee827110c'),
-(4, 'qweqweq', 'qweqweqwe@sadasdsd.com', 'a8f5f167f44f4964e6c998dee827110c'),
-(5, '123123', '123@123.com', '202cb962ac59075b964b07152d234b70'),
-(6, 'novi', 'novi@gmail.com', 'afd4b9acf8ae6a9e99d2d01e5132070e');
+(6, 'Najnoviji', 'noviji@gmail.com', 'afd4b9acf8ae6a9e99d2d01e5132070e');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
